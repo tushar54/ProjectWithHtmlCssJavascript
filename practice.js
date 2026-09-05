ee56987a8781b7535ec9a30c9a76a61e31b13dd7
Tushar ahmed <tusharahmed7083@gmail.com>
2026-09-05 22:15:29 +0600

    i am practicing JavaScript

---
+++ practice.js
@@ -0,0 +1,20 @@
+/**Scope chain*/
+// let name = 'tushar'
+// function outer() {
+//     let age = 20
+    
+//     function inner(){
+//         let city = 'Baoikhola'
+//         console.log(city, age, name)
+
+//     }
+//     inner()
+// }
+
+// outer()
+
+//**Hoisting**/
+
+console.log(name);
+
+var name = "Tushar";
